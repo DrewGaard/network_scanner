66b811a186b5af39ffe5c0e872e29ec408943ad0
DrewGaard <@Mustangs197>
2018-10-14 14:53:53 -0400

    Accessing dictionary elements by key

--- network_scanner.py
+++ network_scanner.py
@@ -9,7 +9,6 @@ def scan(ip):
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     #print(answered_list.summary())
 
-    print("IP\t\t\tMAC Address\n------------------------------------------------")
 
     clients_list = []
 
@@ -17,9 +16,15 @@ def scan(ip):
     for element in answered_list:
         client_dict = {"ip":element[1].psrc, "mac":element[1].hwsrc}
         clients_list.append(client_dict)
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
-    print(clients_list)
+    return clients_list
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n------------------------------------------------")
+
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
 
-scan("10.0.2.1/24")
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
