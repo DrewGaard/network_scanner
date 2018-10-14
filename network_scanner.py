261516518f70ef7d4d245b09e03b54a68ef3b7d5
DrewGaard <@Mustangs197>
2018-10-14 17:31:21 -0400

    Using the optparse module to allow the user to input ip range

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
+    (options, arguments) = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -8,10 +15,7 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     #print(answered_list.summary())
-
-
     clients_list = []
-
 
     for element in answered_list:
         client_dict = {"ip":element[1].psrc, "mac":element[1].hwsrc}
@@ -26,5 +30,7 @@ def print_result(results_list):
         print(client["ip"] + "\t\t" + client["mac"])
 
 
-scan_result = scan("10.0.2.1/24")
+options = get_arguments()
+
+scan_result = scan(options.target)
 print_result(scan_result)
